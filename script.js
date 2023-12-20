27efb738db7f7cce64088f7158645c36ffc91d13
Vedansh_Rashinkar <112638446+Vedansh404@users.noreply.github.com>
2023-12-20 18:49:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 'use strict';
+//selecting Elements
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
@@ -38,6 +39,7 @@ const changeImage = function(dice){
     document.querySelector('img').src=`dice-${dice}.png`;
 }
 
+//switching player
 const switchPlayer=function(){
     document.getElementById(`current--${activePlayer}`).textContent=0;
     
@@ -47,14 +49,19 @@ const switchPlayer=function(){
     player1El.classList.toggle('player--active');    
 }
 
+// rolling dice functionality
 document.querySelector('.btn--roll').
 addEventListener('click',function(){
+    //if playing is  true
     if(playing){
+        // generate random no. between 1 to 6.
         let dice=Math.trunc(Math.random()*6)+1;
-        console.log(dice);
+        // console.log(dice);
+        
         changeImage(dice);
-
+        //checking whether dice has 1 or not as a output.
         if(dice!==1){
+            // increasing the current score.
             currentScore+=dice;
             document.getElementById(`current--${activePlayer}`).textContent=currentScore;
         }else{
@@ -63,14 +70,15 @@ addEventListener('click',function(){
     }    
 });
 
-
+// hold button functionality
 document.querySelector('.btn--hold').addEventListener('click',function(){
 
     if(playing){
+        // updating the score for respective active player
         scores[activePlayer]+=currentScore;
 
         document.getElementById(`score--${activePlayer}`).textContent=scores[activePlayer];
-        
+        // checking the winning condition
         if(scores[activePlayer]>=100){
             playing=false;
             diceEl.classList.add('hidden');
@@ -87,7 +95,7 @@ document.querySelector('.btn--hold').addEventListener('click',function(){
 });
 
 
-
+// new game funtionality
 document.querySelector('.btn--new').addEventListener('click',init);
 
 
